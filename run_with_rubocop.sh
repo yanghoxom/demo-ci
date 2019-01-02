d82bf730fd6b131724a8de93b636971484cf9fea
memsenpai <memsenpai3@gmail.com>
2019-01-02 13:42:09 +0700

    v2

--- run_with_rubocop.sh
+++ run_with_rubocop.sh
@@ -8,7 +8,6 @@ err=$( git fetch origin master && git diff -z --name-only FETCH_HEAD.. \
 echo $err
 mess=`bundle exec ruby parse_rubocop_xml.rb $err`
 echo $mess
-print "$err"
 if [ "$mess" ]; then
   POST_BODY="{\"body\": \"RUBOCOP WARNING!!! \n $mess  \n\n $CIRCLE_BUILD_URL\"}"
   curl -XPOST \
@@ -17,3 +16,23 @@ if [ "$mess" ]; then
     -d "$POST_BODY" \
     https://api.github.com/repos/memsenpai/demo-ci/issues/${CI_PULL_REQUEST##*/}/comments
 fi
+
+# # @drone_link = ARGV[0]
+# # @commit_id = ARGV[1]
+
+# pull_number=${CI_PULL_REQUEST##*/}
+# echo $pull_number
+# # @github_token = ARGV[3]
+
+# mess=$(bundle exec ruby auto_comment.rb $CIRCLE_BUILD_URL $CIRCLE_SHA1 $pull_number $GITHUB_ACCESS_TOKEN)
+# echo $mess
+# if [ "$mess" ]; then
+#   curl -X POST \
+#   -H "Authorization: token $GITHUB_ACCESS_TOKEN"\
+#   -H "Content-Type: application/json"\
+#   -d "{\
+#         \"body\": \":warning: :warning: :warning: **RUBOCOP WARNING!!!** PLEASE CHECK IT AGAIN \",\
+#         \"event\": \"COMMENT\",\
+#         \"comments\": $mess \
+#   }" -v https://api.github.com/repos/memsenpai/demo-ci/pulls/$pull_number/reviews
+# fi
